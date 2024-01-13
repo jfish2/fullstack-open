57524770a02386cd2e1dc41e72bf68aa0589c306
Jonathan Fisher <jonathan@trueanthem.com>
2024-01-12 22:04:58 -0800

    1.5 course information step5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,12 +1,11 @@
 
 const Header = (props) => {
     return (
-            <h1>{props.course}</h1>
+            <h1>{props.course.name}</h1>
         )
 }
 
 const Part = (props) => {
-    console.log(props)
     return (
             <p>
                 {props.part.name} {props.part.exercises}
@@ -35,27 +34,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <>
             <Header course={course} />
-            <Content parts={parts}/>
-            <Total parts={parts} />
+            <Content parts={course.parts}/>
+            <Total parts={course.parts} />
         </>
     )
 }
